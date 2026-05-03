77a7387f2cca8981c3942e86e5dd5b734037e029
root <eng.h.elnagar@gmail.com>
2026-05-03 03:51:36 +0000

    Update Python launch file: integrated server and client nodes using xterm for interactive UI

--- nav_action_py/launch/navigation_py.launch.py
+++ nav_action_py/launch/navigation_py.launch.py
@@ -3,10 +3,20 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # run server in backgroungd
         Node(
             package='nav_action_py',
             executable='server',
             name='navigation_server_py',
             output='screen'
+        ),
+        
+        # open xterm 
+        Node(
+            package='nav_action_py',
+            executable='client',
+            name='navigation_client_py',
+            output='screen',
+            prefix=['xterm -e']  
         )
     ])
